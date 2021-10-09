078d7ed53d68cc39543ae7c6b787116f537b2443
sowrav <sowravroyjoy@gmail.com>
2021-10-09 21:17:54 +0600

    Day 21 completed

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -15,13 +15,13 @@ class CatalogList extends StatelessWidget {
       shrinkWrap: true,
       itemCount: CatalogModel.items!.length,
       itemBuilder: (context, index) {
-        final catalog = CatalogModel.items![index];
+        final catalog = CatalogModel.getByPosition(index);
         return InkWell(
           onTap: () => Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => HomeDetailPage(
-                      catalog: catalog
+                      catalog: catalog,
                   )
               )
           ),
