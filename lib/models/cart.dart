9f2a80ebf31dce39a6836c4017ff0d3f51e15ec1
sowrav <sowravroyjoy@gmail.com>
2021-10-15 14:45:44 +0600

    Day 28 completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -3,7 +3,6 @@ import 'package:first_flutter_project/models/catalog.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-
   //catalog field
   CatalogModel? _catalog;
 
@@ -24,18 +23,26 @@ class CartModel {
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
 
-  //remove Item
-  void remove(Item item) {
-    _itemIds.remove(item.id);
-  }
 }
 
-class AddMutation extends VxMutation<MyStore>{
+class AddMutation extends VxMutation<MyStore> {
   final Item item;
+
   AddMutation(this.item);
+
   @override
   perform() {
     store!.cart!._itemIds.add(item.id);
   }
+}
 
+class RemoveMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  RemoveMutation(this.item);
+
+  @override
+  perform() {
+    store!.cart!._itemIds.remove(item.id);
+  }
 }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -34,7 +34,16 @@ class _CartTotal extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart!.totalPrice}".text.xl5.color(context.theme.accentColor).make(),
+          VxBuilder(
+            mutations: {RemoveMutation},
+            builder: (context, mutations, _) {
+              return "\$${_cart!.totalPrice}"
+                  .text
+                  .xl5
+                  .color(context.theme.accentColor)
+                  .make();
+            },
+          ),
           30.widthBox, 
           ElevatedButton(onPressed: (){
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: "Buying not supported yet".text.make(),
@@ -53,6 +62,7 @@ class _CartList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
     final CartModel? _cart = (VxState.store as MyStore).cart;
     return _cart!.items.isEmpty? "Nothing to show".text.xl3.makeCentered()
    : ListView.builder(
@@ -61,10 +71,7 @@ class _CartList extends StatelessWidget {
           leading: Icon(Icons.done),
           trailing: IconButton(
             icon: Icon(Icons.remove_circle_outline),
-            onPressed: (){
-              _cart.remove(_cart.items[index]);
-              //setState(() {});
-            },
+            onPressed: () => RemoveMutation(_cart.items[index]),
           ),
           title: _cart.items[index].name.text.make(),
         ),
